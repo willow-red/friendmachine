3a742608b8d788ae5aa02b9deb2e76e234a9f47c
Red <saraalva@usc.edu>
2021-12-01 12:55:24 -0800

    Hearts spam animation at max level

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ var heart = document.getElementById("heart");
 var message = document.getElementById("message");
 var facePic = document.getElementById("face")
 
+var heart1 = document.getElementById("heartSpam1");
+var heart2 = document.getElementById("heartSpam2");
+var heart3 = document.getElementById("heartSpam3");
+
 var numLikes = 0;
 var MAX_LEVEL = 10;
 var LIKES_TO_LVLUP = 20;
@@ -21,8 +25,28 @@ function liked(){
   var p = document.getElementById("p");
   sendMessage("friendship level " + levelNum + "! " + likesToNextLvl);
   if(levelNum >= MAX_LEVEL){
-          sendMessage("max friendship level reached !");
+    sendMessage("max friendship level reached !");
   }
+
+  // spam hearts
+  if(levelNum >= MAX_LEVEL){
+    // heart1:
+    heart1.className = ""
+    heart1.style["animation-name"] = "liked"
+    heart1.style["animation-duration"] = "1s infinite"
+    heart1.className = "spam";
+    // heart2:
+    heart2.className = ""
+    heart2.style["animation-name"] = "liked"
+    heart2.style["animation-duration"] = "1s infinite"
+    heart2.className = "spam";
+    // heart3:
+    heart3.className = ""
+    heart3.style["animation-name"] = "liked"
+    heart3.style["animation-duration"] = "1s infinite"
+    heart3.className = "spam";
+  }
+
 
   // heart animation
   var h = heart.cloneNode()
@@ -32,11 +56,11 @@ function liked(){
 
   h.className = "animate";
   document.getElementById("game").appendChild(h)
-	
+
   // delete the heart when the animation is done to prevent clutter
   h.addEventListener("animationend", function(){
   	h.remove()
-  }); 
+  });
 }
 
 function sendMessage(msg){
@@ -44,6 +68,7 @@ function sendMessage(msg){
 }
 
 function newComment(event){
+  var levelMessage = ""
 	if (event.keyCode == 13){ //if enter was pressed
 		var p = document.getElementById("p")
 		var commentsBox = document.getElementById("comments")
@@ -53,6 +78,5 @@ function newComment(event){
 }
 
 //events!!
-
 message.addEventListener("keydown",newComment)
 document.getElementById("btn").addEventListener("click", liked)
